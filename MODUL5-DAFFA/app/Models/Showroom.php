149022c6421ebac07bd550d5edc20e87e694154f
mdaffakhairy <upay.papay@gmail.com>
2023-12-06 08:42:19 +0700

    MODUL5-DAFFA

---
+++ MODUL5-DAFFA/app/Models/Showroom.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Showroom extends Model
+{
+    use HasFactory;
+
+    public $table = 'showroom mobil';
+    protected $fillable = [ 
+        'nama_mobil', 'brand_mobil', 'warna_mobil', 'tipe mobil', 'harga_mobil'
+];
+}
